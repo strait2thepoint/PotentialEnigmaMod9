9bfff7c0048e5c6945e83b85aa71c19012f5e9bd
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-04-09 15:56:39 -0400

    added code to js files, still have error message on line 62 of genMD

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markDown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'Project title',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Project description?'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Project usage?' 
+}
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Contribution information?'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'For questions(email)?'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'For questions(github)?'
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'License Type?'
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers)=>{
+        const mark = markDown.generateReadMe(answers)
+        console.log(mark)
+        fs.writeFile('README.md', mark, 
+        function(err)=>{
+            if(err){
+                console.log('Could not save file')
+            } else {
+              console.log('Success: new README generated inside the current folder')  
+            }
+        })
+        return answers
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,74 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markDown = require('./generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'Project title',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Project description?'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Project usage?' 
+}
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Contribution information?'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'For questions(email)?'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'For questions(github)?'
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'License Type?'
+}
+];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+// TODO: Create a function to initialize app
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers)=>{
+        const mark = markDown.generateReadMe(answers)
+        console.log(mark)
+        fs.writeFile('README.md', mark, 
+        function(err)=>{
+            if(err){
+                console.log('Could not save file')
+            } else {
+              console.log('Success: new README generated inside the current folder')  
+            }
+        })
+        return answers
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 }
 
-module.exports = generateMarkdown;
+// Function call to initialize app
+init();
