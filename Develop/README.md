1858e64b0d716b5a28d65cc4829a360e806c68b3
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-04-13 14:57:17 -0400

    fixed README

---
+++ Develop/README.md
@@ -0,0 +1,29 @@
+# 123
+
+[![License: GNU](https://img.shields.io/badge/GNU-strait2thepoint-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
+## Table of Content
+-[Project description](#Description)
+-[Usage](#Usage)
+-[Contribution](#Contribution)
+-[Installation](#Installation)
+-[Questions](#Questions)
+-[License](#License)
+
+## Description
+123
+
+## Usage
+12
+
+## Installation
+123
+
+## Contributing
+3
+
+## Questions
+123
+123
+
+## License
+Licensed under the [GNU](https://choosealicense.com/licenses/gpl-3.0/) license
--- Develop/index.js
+++ Develop/index.js
@@ -4,80 +4,97 @@ const fs = require('fs');
 const path = require('path')
 const markDown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
+
+class Question {
+    constructor(name, message) {
+        this.name = name
+        this.message = message
+    }
+}
+
+class InputQuestion extends Question {
+    constructor(name, message) {
+        super(name, message)
+        this.type = "input"
+    }
+}
+
+class ListQuestion extends Question {
+    constructor(name, message, choices) {
+        super(name, message)
+        this.type = "list",
+            this.choices = choices
+    }
+}
+
 const questions = [
-{
-    type: 'input',
-    name: 'title',
-    message: 'Project title:',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Project description:'
-},
-{
-    type: 'input',
-    name: 'installation',
-    message: 'Installation instructions:'
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: 'Project usage instructions:' 
-},
-{
-    type: 'input',
-    name: 'contribution',
-    message: 'Contribution information:'
-},
-{
-    type: 'input',
-    name: 'questions',
-    message: 'For questions(email):'
-},
-{
-    type: 'input',
-    name: 'questions',
-    message: 'For questions(github):'
-},
-{
-    type: 'input',
-    name: 'license',
-    message: 'License Type:',
-    choices: [
+    new InputQuestion("title", "What's the title of your project"),
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project usage instructions:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution information:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'For questions(email):'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'For questions(github):'
+    },
+    new ListQuestion("license", "License type:", [
         'No license',
         'MIT',
         'ISC',
         'GNU',
-    ]
-}
+    ])
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(),fileName), data)
-} 
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
-     inquirer.prompt(questions).then((answers)=>{
+    inquirer.prompt(questions).then((answers) => {
         console.log(answers)
-       
-        fs.createWriteStream('Generating README.md',  
-        function(err) {
-            if(err){
-                console.log('Could not save file')
-            } else {
-              console.log('Success: new README generated inside the current folder')  
-            }
+
+        fs.createWriteStream('Generating README.md',
+            function (err) {
+                if (err) {
+                    console.log('Could not save file')
+                } else {
+                    console.log('Success: new README generated inside the current folder')
+                }
+            })
+        writeToFile('README.md', markDown({ ...answers }))
+
+    })
+        .catch((error) => {
+            console.log(error)
         })
-        writeToFile('README.md', markDown({...answers}))
-        
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
 }
 
 // Function call to initialize app
 init();
+
+
+
